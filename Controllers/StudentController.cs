51d370aba54cefd56b4b4918837936113b4f410f
Palak-Singla <singlapalak34@gmail.com>
2020-04-03 11:18:38 +0530

    second commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gitIgnoreEx.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gitIgnoreEx.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> objstu = new List<Student>()
+            {
+                new Student{SId=1,SName="Palak",SRollNo=101},
+                 new Student{SId=2,SName="Kareena",SRollNo=102},
+                  new Student{SId=3,SName="Pushp",SRollNo=103},
+            };
+            return View(objstu);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gitIgnoreEx.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public int SRollNo { get; set; }
+    }
+}
